cafc79fc16f6f96318e37645556ab1baeeea2cba
hackerboi69 <77713906+hackerboi72@users.noreply.github.com>
2021-07-17 08:37:40 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,16 @@
+console.log("Lol")
+var name = "shourya"
+var name2 = "jain"
+var fullname = name + name2 
+console.log(fullname)
+console.log("ok this is Multiply, divide , add and subtract of 69 and 420.")
+var n = 69
+var n2 = 420
+var add = n + n2
+console.log(add)
+var subtract = n2 - n 
+console.log(subtract)
+var multiply = n * n2
+console.log(multiply)
+var divide = n2 / n 
+console.log(divide)
